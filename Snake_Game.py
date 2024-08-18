5aa21176c19e8d81c6d3f74bd71538300e611676
Russell <russell.d.chubb@gmail.com>
2024-08-19 08:10:11 +1200

    (Release Beta 1.0) My first functional release of the Olivia Catepillar Game

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,7 +1,4 @@
-# To do//
-# Replace SNAKE_COLOUR with image of Olivia 
-
-# importing necesary libraries
+# importing necessary libraries
 from tkinter import *
 from PIL import Image, ImageTk
 import random
@@ -13,8 +10,8 @@ class Config:
     GAME_SPEED = 80
     SPACE_SIZE = 50
     BODY_PARTS = 3
-    SNAKE_IMAGE_PATH = 'olivia-removebg-preview.png'
-    FOOD_IMAGE_PATH = "pear-png-38695.png"
+    SNAKE_IMAGE_PATH = 'olivia.png'
+    FOOD_IMAGE_PATH = "pear.png"
     BACKGROUND_COLOUR = "#000000"
 
 # classes
@@ -24,37 +21,44 @@ class Snake:
         self.cords = []
         self.squares = []
 
-        for i in range (0, Config.BODY_PARTS):
-            self.cords.append([0,0])
+        for i in range(Config.BODY_PARTS):
+            self.cords.append([0, 0])
 
+        # Load and resize the snake image
         self.image = Image.open(Config.SNAKE_IMAGE_PATH)
-        self.image = self.image.resize((Config.SPACE_SIZE, Config.SPACE_SIZE), Image.ANTIALIAS)
+        self.image = self.image.resize((Config.SPACE_SIZE, Config.SPACE_SIZE), Image.LANCZOS)
         self.photo = ImageTk.PhotoImage(self.image)
 
+        # Create image representations of the snake
         for x, y in self.cords:
             square = canvas.create_image(x, y, image=self.photo, anchor=NW, tag="Snake")
             self.squares.append(square)
 
-        for x, y in self.cords:
-            square = canvas.create_rectangle(x, y, x + Config.SPACE_SIZE, y + Config.SPACE_SIZE, fill= Config.SNAKE_IMAGE_PATH, tag="Snake")
-            self.squares.append(square)
+    def update_position(self):
+        # Update the position of each part of the snake
+        for i, (x, y) in enumerate(self.cords):
+            canvas.coords(self.squares[i], x, y)
 
 class Food:
     def __init__(self) -> None:
-        x = random.randint(0, (Config.GAME_WIDTH / Config.SPACE_SIZE)-1) * Config.SPACE_SIZE
-        y = random.randint(0, (Config.GAME_HEIGHT / Config.SPACE_SIZE)-1) * Config.SPACE_SIZE
+        # Ensure that the values used in randint are integers
+        max_x = (Config.GAME_WIDTH // Config.SPACE_SIZE) - 1
+        max_y = (Config.GAME_HEIGHT // Config.SPACE_SIZE) - 1
+        
+        x = random.randint(0, max_x) * Config.SPACE_SIZE
+        y = random.randint(0, max_y) * Config.SPACE_SIZE
 
-        self.cords = [x,y]
+        self.cords = [x, y]
 
+        # Load and resize the food image
         self.image = Image.open(Config.FOOD_IMAGE_PATH)
-        self.image = self.image.resize((Config.SPACE_SIZE, Config.SPACE_SIZE), Image.ANTIALIAS)
+        self.image = self.image.resize((Config.SPACE_SIZE, Config.SPACE_SIZE), Image.LANCZOS)
         self.photo = ImageTk.PhotoImage(self.image)
 
-        canvas.create_image(x, y, x, image = self.photo, anchor=NW, tag="food")
+        canvas.create_image(x, y, image=self.photo, anchor=NW, tag="food")
 
 # functions
 def next_turn(snake, food):
-    
     x, y = snake.cords[0]
 
     if direction == "up":
@@ -68,21 +72,16 @@ def next_turn(snake, food):
 
     snake.cords.insert(0, (x, y))
 
-    square = canvas.create_rectangle(x, y, x + Config.SPACE_SIZE, y + Config.SPACE_SIZE, fill = Config.SNAKE_IMAGE_PATH)
-
+    square = canvas.create_image(x, y, image=snake.photo, anchor=NW, tag="Snake")
     snake.squares.insert(0, square)
 
     if x == food.cords[0] and y == food.cords[1]:
-
         global score
         score += 1
-
         label.config(text="Score: {}".format(score))
 
         canvas.delete("food")
-
         food = Food()
-
     else:
         del snake.cords[-1]
         canvas.delete(snake.squares[-1])
@@ -94,20 +93,20 @@ def next_turn(snake, food):
     window.after(Config.GAME_SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
-        global direction
+    global direction
 
-        if new_direction == 'left':
-            if direction != 'right':
-                direction = new_direction
-        elif new_direction == 'right':
-            if direction != 'left':
-                direction = new_direction
-        elif new_direction == 'up':
-            if direction != 'down':
-                direction = new_direction
-        elif new_direction == 'down':
-            if direction != 'up':
-                direction = new_direction
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
 
 def check_collisions(snake):
     x, y = snake.cords[0]
@@ -133,25 +132,26 @@ def restart_game():
     food = Food()
     score = 0
     direction = "down"
-    label.config(text="Score:{}".format(score))
+    label.config(text="Score: {}".format(score))
     next_turn(snake, food)
-
-# drawing the restart button
-restart_button = Button(Window, text="Restart", command = restart_game, font=("Arial", 20))
-restart_button.place(x=0,y=0)
 
 # calling the window
 window = Tk()
-window.title = "Snake Game!"
+window.title("Snake Game!")
 window.resizable(False, False)
-window.attributes('-fullscreen', True)
+window.attributes('-fullscreen', False)
 
+# Initialize score and direction variables
 score = 0
 direction = "right"
 
+# drawing the restart button
+restart_button = Button(window, text="Restart", command = restart_game, font=("Arial", 20))
+restart_button.place(x=0, y=0)
+
 # creating a label on the screen
-label = Label(window, text="Score{}".format(score), font=("arial", 40)) # I don't understand how the format method works? 
-label.pack() # I don't understand how packing works either?
+label = Label(window, text="Score: {}".format(score), font=("Arial", 40))
+label.pack()
 
 canvas = Canvas(window, bg = Config.BACKGROUND_COLOUR, height = Config.GAME_HEIGHT, width = Config.GAME_WIDTH)
 canvas.pack()
